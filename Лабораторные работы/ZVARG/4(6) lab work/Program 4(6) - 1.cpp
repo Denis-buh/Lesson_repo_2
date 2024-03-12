0ebfcee30cc2adbd661913af69442c9f9efd2b02
Denis <denisbuhanov4@gmail.com>
2024-03-13 08:19:05 +0900

    Что-то от Влада

---
+++ Лабораторные работы/ZVARG/4(6) lab work/Program 4(6) - 1.cpp
@@ -0,0 +1,74 @@
+﻿/*
+10. В функции подсчитать количество отрицательных элементов одномерного массива, порядковый номер которых меньше номера минимального
+элемента. Использовать функцию для каждой строки двумерного массива
+размерности 15 на 15.
+
+
+Задание. В соответствии с вариантом создать функцию, параметрами
+которой являются: одномерный массив и его размерность.
+Функцию реализовать двумя способами:
+1) через обращение к элементам массива через указатели,
+2) обычным способом.
+Оба способа должны различаться: видами записи формальных и фактических параметров функции, обращением к элементам массива.
+Можно создать два экземпляра программы, либо выполнить одну программу с разными функциями.
+Продемонстрировать возможность применения созданной функции к
+строкам двумерного массива. Элементы двумерного массива задать случайным образом, не забыв вывести их на экран.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int min_finding(int* arr)//функция по нахождению минимального из массива 
+{
+	int min(*arr);
+	for (int el_index = 0; el_index < 14; el_index++)
+	{
+		if (min > *(arr + el_index + 1)) min = *(arr + el_index + 1);
+	}
+	return min;
+}
+
+int arr_view(int* arr)// функция для вывода массива 
+{
+	cout << "1-dim array: " << endl;
+	for (int el_index = 0; el_index < 15; el_index++)
+	{
+		cout << setw(3) << *(arr + el_index) << ';';
+	}
+	cout << endl;
+	return 0;
+}
+
+int main()
+{
+	int main_array[15][15];
+	int* main_array_pointer = &main_array[0][0];
+	srand(time(0)); //инициализируем рандомайзер
+	for (int el_index = 0; el_index < 225; el_index++)//цикл, что вносит в массив случ. числа
+	{
+		*(main_array_pointer + el_index) = (rand() % 11) - 5;//непосредственно внос рандомных чисел
+	}
+	for (int arr_index = 0; arr_index < 15; arr_index++)
+	{
+		int min;
+		int sum = 0;
+		arr_view(*(main_array + arr_index));//вывод "рабочего" одномерного массива
+		min = min_finding(*(main_array + arr_index));//нахождение минимального из "рабочего" одномерного массива
+		for (int el_index = 0; el_index < 15; el_index++)
+		{
+			if (*(*(main_array + arr_index) + el_index) == min)
+			{
+				break;
+			}
+			if (*(*(main_array + arr_index) + el_index) < 0)
+			{
+				sum += *(*(main_array + arr_index) + el_index);
+			}
+		}
+		cout << "Sum in this 1 - dim array: " << sum << ", Min : " << min << endl;
+	}
+	system ("pause");
+	return 0;
+}
---
+++ Лабораторные работы/ZVARG/5(7) lab work/Program 5(7).cpp
@@ -0,0 +1,43 @@
+﻿/*
+10. Создать функцию для определения значений синуса и косинуса углов
+прямоугольного треугольника. Параметры функции – длины катетов. Вывести
+результаты работы функции из основной программы.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int result_finding(float& adj,float& opp, double& hypo)//adj выступает в качестве cos, opp - в качестве sin
+{
+	float temp_param;
+	temp_param = adj;
+	opp = opp / hypo;
+	adj = adj / hypo;
+	return 0;
+}
+int main()
+{
+	float a;
+	float& a_link = a;
+	float b;
+	float& b_link = b;
+	double c;
+	double& c_link = c;
+	float result = 0;
+	cout << "Enter a (adjacent leg) = ";
+	cin >> a;
+	cout << endl;
+	cout << "Enter b (opposite leg) = ";
+	cin >> b;
+	cout << endl;
+	c = sqrt (a*a + b*b);
+	cout << "Hypotenuse = ";
+	cout << c;
+	cout << endl;
+	result_finding(a_link, b_link, c_link);
+	cout << "Sin = " << b_link << ", Cos = " << a_link << endl;
+	system("pause");
+	return 0;
+}
