cfa10e5ae4e199f7ef15abb758277096297ee1fd
Denis <denisbuhanov4@gmail.com>
2024-03-13 22:57:05 +0900

    Add files via upload

--- Лабораторные работы/Denis-buh/№ 8/foot_comand.cpp
+++ Лабораторные работы/Denis-buh/№ 8/foot_comand.cpp
@@ -11,6 +11,8 @@
 
 
 #include <iostream>
+#include <iomanip> // Для форматного вывода
+
 
 using namespace std; 
 
@@ -23,15 +25,35 @@ private:
     string city;
     // количество набранных очков 
     float points;
+
 public:
     // Инициализируем объект структуры
     Football_comand(string name_command, string city, float points){
         // this - обьект структуры Football_comand
-        this->name_command = name_command;
-        this->city = city;
-        this->points = points; 
+        this->name_command =  string(name_command);
+        this->city =  string(city);
+        this->points =  float(points); 
     }
-
     Football_comand(){
     }
+    Football_comand(Football_comand& obj){
+        this->name_command =  string(obj.name_command);
+        this->city =  string(obj.city);
+        this->points =  float(obj.points); 
+    }
+
+    // Гетеры
+    string get_name_command(){return this->name_command;}
+    string get_city(){return this->city;}
+    float get_points(){return this->points;}
+
+
+    void fprint(){
+        cout << setw(20) << this->name_command << "|";
+        cout << setw(20)  << this->city << "|";
+        cout << setw(20)  << this->points << "|";
+        cout << "\n";
+    }
+
+
 };
