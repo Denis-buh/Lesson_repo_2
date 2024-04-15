6f5895aabe924b52e5b7c3c0f61f18c6b3741ed3
Denis <denisbuhanov4@gmail.com>
2024-04-15 20:20:43 +0900

    Add files via upload

---
+++ Конспекты/Denis-buh/UML/UML схемы/Диаграмма классов/0) Общее/test.cpp
@@ -0,0 +1,54 @@
+
+
+
+
+#include "iostream"
+
+
+using namespace std;
+
+
+class Abstract_obj{
+protected:
+    string name; // имя
+    int age; // возраст
+public:
+    // КОНСТРУКТОРЫ
+    // Конструктор с отложенной инициализацией
+    Abstract_obj(){}
+    // Конструктор с инициализацией
+    Abstract_obj(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+
+    // СЕТЕРЫ
+	// сетер name
+    void set_name(string name){this->name = name;}
+	// сетер age
+    void set_age(int age){this->age = age;}
+	// сетер для name и age
+    void set_inform(string name, int age){
+        this->set_name(name);
+        this->set_age(age);
+    }
+
+    // ГЕТЕРЫ
+	// гетер name
+    string get_name(){return this->name;}
+	// гетер age
+    int get_age(){return this->age;}
+	// гетер name и age
+    void get_inform(string& name, int& age){
+        name = this->get_name(); 
+        age = this->get_age(); 
+    }
+};
+
+
+
+int main(){
+    system("chcp 1251"); 
+
+    return 0; 
+}
